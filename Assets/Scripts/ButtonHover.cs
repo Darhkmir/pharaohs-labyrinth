448e7dbc1a0037d159d7cba600e8fd640bbf5d54
PixlCrim <schempersjacobus16@gmail.com>
2021-10-05 15:51:42 +0200

    Unity Project Files
    The unity project before using GitHub, it contains an animated pixel art title screen.
    
    New button doesn't work, Load and Options buttons lead to "Unavailable" text. The credits button works with animations.

---
+++ Assets/Scripts/ButtonHover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+	
+	public Text theText;
+	
+	public void OnPointerEnter(PointerEventData eventData) {
+		theText.color = new Color32(236, 0, 95, 255);
+	}
+	
+	public void OnPointerExit(PointerEventData eventData) {
+		theText.color = new Color32(94, 32, 116, 255);
+	}
+	
+	public void ResetHover() {
+		theText.color = new Color32(94, 32, 116, 255);
+	}
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+	
+	public GameObject canvas;
+	public GameObject main;
+	public GameObject credits;
+	public GameObject notAvailable;
+	
+	Animator mainAnimator;
+	Animator creditsAnimator;
+	
+	public void Start() {
+		mainAnimator = main.GetComponent<Animator>();
+		creditsAnimator = credits.GetComponent<Animator>();
+	}
+	
+	public void CreditsButton() {
+		StartCoroutine(DisableMain(0.4f));
+		credits.SetActive(true);
+	}
+	
+	public void CreditsBackButton() {
+		StartCoroutine(DisableCredits(0.5f));
+		main.SetActive(true);
+		
+	}
+    
+    public void ExitButton() {
+		Application.Quit();
+    }
+	
+	IEnumerator DisableMain(float delay) {
+		mainAnimator.SetTrigger("Close");
+		yield return new WaitForSeconds(delay);
+		main.SetActive(false);
+	}
+	
+	IEnumerator DisableCredits(float delay) {
+		creditsAnimator.SetTrigger("Close");
+		yield return new WaitForSeconds(delay);
+		credits.SetActive(false);
+	}
+}
