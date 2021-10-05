1816c38e617f54ff09fc3a3c5708caa68b0fdaa6
PixlCrim <schempersjacobus16@gmail.com>
2021-10-05 17:44:02 +0200

    Title Screen Animations pt. 2
    Added an opening and closing animation to the title text and shortened the delay after the credits closing animation by 0.10 seconds.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,22 +9,26 @@ public class MainMenu : MonoBehaviour {
 	public GameObject main;
 	public GameObject credits;
 	public GameObject notAvailable;
+	public GameObject title;
 	
 	Animator mainAnimator;
 	Animator creditsAnimator;
+	Animator titleAnimator;
 	
 	public void Start() {
 		mainAnimator = main.GetComponent<Animator>();
 		creditsAnimator = credits.GetComponent<Animator>();
+		titleAnimator = title.GetComponent<Animator>();
 	}
 	
 	public void CreditsButton() {
-		StartCoroutine(DisableMain(0.4f));
+		StartCoroutine(DisableMain(0.5f));
 		credits.SetActive(true);
 	}
 	
 	public void CreditsBackButton() {
-		StartCoroutine(DisableCredits(0.5f));
+		StartCoroutine(DisableCredits(0.4f));
+		title.SetActive(true);
 		main.SetActive(true);
 		
 	}
@@ -34,9 +38,11 @@ public class MainMenu : MonoBehaviour {
     }
 	
 	IEnumerator DisableMain(float delay) {
+		titleAnimator.SetTrigger("Close");
 		mainAnimator.SetTrigger("Close");
 		yield return new WaitForSeconds(delay);
 		main.SetActive(false);
+		title.SetActive(false);
 	}
 	
 	IEnumerator DisableCredits(float delay) {
