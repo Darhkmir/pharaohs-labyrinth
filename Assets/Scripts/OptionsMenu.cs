fbf2495123ca9d251fe9415cf0bd9eca17ebc9c4
PixlCrim <schempersjacobus16@gmail.com>
2021-10-18 10:57:34 +0200

    Options Saving
    Playerprefs implemented with the options menu, so now the options save when you change them, and load back when you open the game.

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -8,10 +8,15 @@ public class OptionsMenu : MonoBehaviour {
 	
 	public AudioMixer audioMixer;
 	public Dropdown resolutionDropdown;
+	public Dropdown graphicsDropdown;
+	public Toggle fullscreenToggle;
+	public Slider volumeSlider;
 	
 	public Resolution[] resolutions;
 	
 	void Start() {
+		GetPlayerPrefs();
+		
 		resolutions = Screen.resolutions;
 		resolutionDropdown.ClearOptions();
 		
@@ -37,18 +42,62 @@ public class OptionsMenu : MonoBehaviour {
 	public void SetResolution (int resolutionIndex) {
 		Resolution resolution = resolutions[resolutionIndex];
 		Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+		PlayerPrefs.SetInt("resWidth", resolution.width);
+		PlayerPrefs.SetInt("resHeight", resolution.height);
 	}
     
 	public void SetVolume (float volume) {
 		audioMixer.SetFloat("volume", volume);
+		PlayerPrefs.SetFloat("volume", volume);
 	}
 	
 	public void SetQuality (int qualityIndex) {
 		QualitySettings.SetQualityLevel(qualityIndex);
+		PlayerPrefs.SetInt("quality", qualityIndex);
 	}
 	
 	public void SetFullscreen (bool isFullscreen) {
 		Screen.fullScreen = isFullscreen;
+		if (isFullscreen == true) {
+			PlayerPrefs.SetString("fullscreen", "true");
+		} else {
+			PlayerPrefs.SetString("fullscreen", "false");
+		}
 	}
-    
+	
+	public void GetPlayerPrefs() {
+		if (!PlayerPrefs.HasKey("volume")) {
+			PlayerPrefs.SetInt("resWidth", 1280);
+			PlayerPrefs.SetInt("resHeight", 720);
+			Screen.SetResolution(1280, 720, Screen.fullScreen);
+			
+			PlayerPrefs.SetFloat("volume", 0f);
+			audioMixer.SetFloat("volume", 0f);
+			volumeSlider.value = 0f;
+			
+			PlayerPrefs.SetInt("quality", 2);
+			QualitySettings.SetQualityLevel(2);
+			graphicsDropdown.value = 2;
+			
+			PlayerPrefs.SetString("fullscreen", "false");
+			Screen.fullScreen = false;
+			fullscreenToggle.isOn = false;
+		} else {
+			int prefsResWidth = PlayerPrefs.GetInt("resWidth");
+			int prefsResHeight = PlayerPrefs.GetInt("resHeight");
+			Screen.SetResolution(prefsResWidth, prefsResHeight, Screen.fullScreen);
+			
+			float prefsVolume = PlayerPrefs.GetFloat("volume");
+			audioMixer.SetFloat("volume", prefsVolume);
+			volumeSlider.value = prefsVolume;
+			
+			int prefsQuality = PlayerPrefs.GetInt("quality");
+			QualitySettings.SetQualityLevel(prefsQuality);
+			graphicsDropdown.value = prefsQuality;
+			
+			bool prefsFullscreen = PlayerPrefs.GetString("fullscreen") == "true";
+			Screen.fullScreen = prefsFullscreen;
+			fullscreenToggle.isOn = prefsFullscreen;
+		}
+	}
 }
