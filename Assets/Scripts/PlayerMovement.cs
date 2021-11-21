e5cc28b23018b7aa84c0cab0c6e69dc754bb0375
PixlCrim <schempersjacobus16@gmail.com>
2021-11-21 19:44:34 +0200

    Tiles + Movement
    Floor tiles and wall tiles, as well as a temporary nude character sprite and a movement system.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	
+	public float moveSpeed = 5f;
+	Vector2 movement;
+	
+	public Rigidbody2D rigidBody;
+	public Animator animator;
+
+    void Update() {
+	    movement.x = Input.GetAxisRaw("Horizontal");
+	    movement.y = Input.GetAxisRaw("Vertical");
+	    
+	    animator.SetFloat("Horizontal", movement.x);
+	    animator.SetFloat("Vertical", movement.y);
+	    
+	    if (movement != Vector2.zero) {
+		    animator.SetFloat("LastX", movement.x);
+		    animator.SetFloat("LastY", movement.y);
+    	}
+	    
+	    animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+    
+	void FixedUpdate() {
+		rigidBody.MovePosition(rigidBody.position + movement * moveSpeed * Time.fixedDeltaTime);
+	}
+    
+}
