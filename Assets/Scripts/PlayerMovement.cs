bce056612bb3cf265e51d9c73c5e8c9276a86bca
PixlCrim <schempersjacobus16@gmail.com>
2021-12-08 11:07:41 +0200

    Grid-Based Movement
    Added a new game icon, my new logo, a shirt and pants for the player, and got the tile movement working, though it led to some collision issues that need to be fixed.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,28 +5,42 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 	
 	public float moveSpeed = 5f;
+	public Transform movePoint;
 	Vector2 movement;
-	
-	public Rigidbody2D rigidBody;
 	public Animator animator;
+	public LayerMask whatStopsMovement;
 
-    void Update() {
-	    movement.x = Input.GetAxisRaw("Horizontal");
-	    movement.y = Input.GetAxisRaw("Vertical");
-	    
-	    animator.SetFloat("Horizontal", movement.x);
-	    animator.SetFloat("Vertical", movement.y);
-	    
-	    if (movement != Vector2.zero) {
-		    animator.SetFloat("LastX", movement.x);
-		    animator.SetFloat("LastY", movement.y);
-    	}
-	    
-	    animator.SetFloat("Speed", movement.sqrMagnitude);
-    }
-    
-	void FixedUpdate() {
-		rigidBody.MovePosition(rigidBody.position + movement * moveSpeed * Time.fixedDeltaTime);
+	void Start() {
+		movePoint.parent = null;
 	}
+
+  void Update() {
+		transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+		movement.x = Input.GetAxisRaw("Horizontal");
+		movement.y = Input.GetAxisRaw("Vertical");
+
+		if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f) {
+			if (Mathf.Abs(movement.x) == 1f) {
+				if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(movement.x, 0f, 0f), 0.2f, whatStopsMovement)) {
+					movePoint.position += new Vector3(movement.x, 0f, 0f);
+				}
+			} else if (Mathf.Abs(movement.y) == 1f) {
+				if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, movement.y, 0f), 0.2f, whatStopsMovement)) {
+					movePoint.position += new Vector3(0f, movement.y, 0f);
+				}
+			}
+		}
+	    
+	  animator.SetFloat("Horizontal", movement.x);
+	  animator.SetFloat("Vertical", movement.y);
+	    
+	  if (movement != Vector2.zero) {
+	    animator.SetFloat("LastX", movement.x);
+	    animator.SetFloat("LastY", movement.y);
+  	}
+	    
+	  animator.SetFloat("Speed", movement.sqrMagnitude);
+  }
     
 }
